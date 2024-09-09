1d8dd355e597f4aa48711a66670c90e858687e95
2carrot84 <2carrot84@gmail.com>
2024-09-09 16:39:05 +0900

    fix: JavaRegex
    정규식 설명 추가

--- src/main/java/org/carrot84/JavaRegex.java
+++ src/main/java/org/carrot84/JavaRegex.java
@@ -16,5 +16,9 @@ public class JavaRegex {
 
 class MyRegex {
 	private final String zeroTo255 = "((0|1)\\d{2}|2[0-4]\\d|25[0-5]|\\d{1,2})";
+	// 1. 0 또는 1 로 시작 하고 2자리 수 숫자
+	// 2. 2 로 시작 하고 0-4 와 1자리 수 숫자
+	// 3. 25 로 시작하고 0-5 숫자
+	// 4. 1 자리나 2 자리 숫자
 	public String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
 }
