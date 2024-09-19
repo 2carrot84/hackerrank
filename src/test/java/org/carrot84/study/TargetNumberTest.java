747c442787a5d2ddc745f2a8ecfbfc05ca62588d
2carrot84 <2carrot84@gmail.com>
2024-09-19 15:55:19 +0900

    fix: 타겟 넘버
    https://school.programmers.co.kr/learn/courses/30/lessons/43165

--- src/test/java/org/carrot84/study/TargetNumberTest.java
+++ src/test/java/org/carrot84/study/TargetNumberTest.java
@@ -1,12 +1,9 @@
 package org.carrot84.study;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
